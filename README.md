6883dddfc4602d2e09091ef7bf0e77943087fb0b
Bao Mai <baomai@berkeley.edu>
2016-11-18 19:26:06 -0800

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # machiavelli
-Application-level schema for Javascript models.
+Lightweight application-level schema for Javascript models.
+
+## Why not Mongoose or another ORM
+ORMs don't cover completely the features that the Official Node.js 
+Driver have. Many ORMs, Mongoose in particular, are slow vs. the 
+official driver and are more prone to bugs.
+
+This module is made for those who are frustrated by reading two docs 
+to communicate with one database. By separating the Schema from the 
+ORM, we are adhering to the Node.js philosophy of modularization.
+
+## Getting Started
+
+```
+var Schema = require('machiavelli').Schema
+var postingSchema = new Schema({
+    title: { type: 'string', required: true },
+    price: { type: 'number', required: true },
+    description: { type: 'string' }
+});
+```
+
+# Synchronous validation
+```
+var posting = { title: 'Old windows mouse', price: 20 };
+if (postingSchema.isValid(posting)) {
+    /* save posting to database */
+} else {
+    /* handle error */ 
+}
+```
+
+# Asynchronous validation
+```
+postingSchema.validate(posting, function (err) {
+    if (err) {
+        /* handle error */
+    } else {
+        /* save posting to database
+    }
+});
+```
+
+# Custom validators 
+```
+var is_positive = function (price) { return price >= 0; };
+var coffeeSchema = new Schema({
+    flavor: { type: 'string', required: true },
+    price: { type: 'number', required: true, validate: [is_positive] }
+});
+```
+
+# Schema inheritance
+The inherits method need to be declared after schema declaration. 
+Will not overwrite existing fields, but will inherit validation 
+requirements of other fields.
+
+```
+var ticketSchema = new Schema({ 
+    artist: { type: 'string', required: 'true' }
+});
+ticketSchema.inherits(postingSchema);
+```
+
+
+## Why use a Schema at all
+Light-weight schemas can provide another protection layer for malicious
+database injections.
+
+## Future contribution ideas
+1. More specific types, like Date, Integer
+2. More tests, please.
+3. Custom error messages for custom validator functions.
---
+++ test/schema-test.js
@@ -0,0 +1,125 @@
+const
+  assert = require('assert'),
+  Schema = require('../lib/machiavelli').Schema;
+
+describe('Schema', function () {
+  const ErrorMessage = {
+    TYPE: 'Invalid type',
+    CUSTOM: 'Custom validator failed',
+    MISSING: 'Missing field: ' /* with custom field name */
+  };
+
+  /*
+   * Test data
+   */
+
+  var valid_user = {
+    username: 'baoser',
+    password: 'foobar',
+    email: 'baoser@berkeley.edu'
+  }, wrong_type_user = {
+    username: 4,
+    password: Date.now(),
+    email: 'baoser@berkeley.edu'
+  }, missing_required_user = {
+    username: 'baoser',
+    password: 'foobar'
+  };
+
+  var no_description_posting = {
+    title: 'Free puppies on Sproul',
+    price: 0
+  }, negative_price_posting = {
+    title: 'Donald Trump',
+    price: -10,
+    description: 'Orange man wanting to be president.' +
+    'Do not leave him alone with woman you love.'
+  }, is_positive = function (price) {
+    return price >= 0;
+  };
+
+  var valid_ticket = {
+    title: "two tickets to Friday's Weezer concert",
+    price: 20,
+    artist: 'Weezer'
+  };
+
+  /*
+   * A test case of a ticketing website
+   */
+
+  var userSchema, postingSchema, ticketSchema;
+  before(function (done) {
+    userSchema = new Schema({
+      username: { type: 'string', required: true },
+      password: { type: 'string', required: true },
+      email: { type: 'string', required: true }
+    });
+    postingSchema = new Schema({
+      title: { type: 'string', required: true },
+      price: { type: 'number', required: true, validate: [is_positive] },
+      description: { type: 'string' }
+    });
+    ticketSchema = new Schema({
+      artist: { type: 'string', required: true }
+    });
+    ticketSchema.inherits(postingSchema);
+    done();
+  });
+
+  it('should have two patterns of validation', function (done) {
+    userSchema.validate(valid_user, function (err) {
+      assert.equal(err, null);
+      assert(userSchema.isValid(valid_user));
+      done();
+    });
+  });
+
+  it('should fail if field is of wrong type', function (done) {
+    userSchema.validate(wrong_type_user, function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.message, ErrorMessage.TYPE);
+      assert(!userSchema.isValid(wrong_type_user));
+      done();
+    });
+  });
+
+  it('should fail if required field is missing', function (done) {
+    userSchema.validate(missing_required_user, function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.message, ErrorMessage.MISSING + 'email');
+      assert(!userSchema.isValid(missing_required_user));
+      done();
+    });
+  });
+
+  it('should pass if non-required field is not specified', function (done) {
+    postingSchema.validate(no_description_posting, function (err) {
+      assert.equal(err, null);
+      assert(postingSchema.isValid(no_description_posting));
+      done();
+    });
+  });
+
+  it('should fail if custom function failed', function (done) {
+    postingSchema.validate(negative_price_posting, function (err) {
+      assert.notEqual(err, null);
+      assert.equal(err.message, ErrorMessage.CUSTOM);
+      assert(!postingSchema.isValid(negative_price_posting));
+      done();
+    });
+  });
+
+  it('should be able to inherit another schema', function (done) {
+    ticketSchema.validate(valid_ticket, function (err) {
+      assert.equal(err, null);
+      assert(!ticketSchema.isValid(no_description_posting));
+      assert(!ticketSchema.isValid(negative_price_posting));
+      assert(ticketSchema.isValid(valid_ticket));
+      done();
+    });
+  });
+
+  it('should not override existing attributes in inheritance');
+});
+
